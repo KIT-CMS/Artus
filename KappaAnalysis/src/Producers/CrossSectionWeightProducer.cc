ab4e0709890eac4d5e4ab14ff502a5c79ef15497
rfriese <raphael.friese@cern.ch>
2017-08-11 15:58:26 +0200

    remove code fragments that are impossible to trigger. The Wrapper prevents the analysis from the GetCrossSection being empty

--- KappaAnalysis/src/Producers/CrossSectionWeightProducer.cc
+++ KappaAnalysis/src/Producers/CrossSectionWeightProducer.cc
@@ -14,10 +14,6 @@ void CrossSectionWeightProducer::Produce( KappaEvent const& event,
 	
 	if (static_cast<double>(settings.GetCrossSection()) > 0.0)
 		product.m_weights["crossSectionPerEventWeight"] = settings.GetCrossSection();
-	else if (event.m_genLumiInfo->xSectionExt > 0.)
-		product.m_weights["crossSectionPerEventWeight"] = event.m_genLumiInfo->xSectionExt;
-	else if (event.m_genLumiInfo->xSectionInt > 0.)
-		product.m_weights["crossSectionPerEventWeight"] = event.m_genLumiInfo->xSectionInt;
 	else
-		LOG(ERROR) << "No CrossSection information found.";
+		LOG(ERROR) << "No CrossSection information in the input found.";
 }
