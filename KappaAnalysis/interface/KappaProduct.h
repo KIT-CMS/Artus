d308ca0b2ff634ce44f60fe5c5ba7e62e3137e92
Maximilian Burkart <maxiburkart@gmail.com>
2018-12-12 14:38:05 +0100

    Add VBF trigger matching.
    Change format of detailed trigger matching for KLV from pointer to copy.
    Save detailed trigger matches.

--- KappaAnalysis/interface/KappaProduct.h
+++ KappaAnalysis/interface/KappaProduct.h
@@ -151,13 +151,13 @@ public:
 
 	/// added by TriggerMatchingProducer
 	// m_detailedTriggerMatchedElectrons[reco lepton][HLT name][filter name] = {trigger objects}
-	std::map<KElectron*, std::map<std::string, std::map<std::string, std::vector<KLV*> > > > m_detailedTriggerMatchedElectrons;
-	std::map<KMuon*, std::map<std::string, std::map<std::string, std::vector<KLV*> > > > m_detailedTriggerMatchedMuons;
-	std::map<KTau*, std::map<std::string, std::map<std::string, std::vector<KLV*> > > > m_detailedTriggerMatchedTaus;
-	std::map<KBasicJet*, std::map<std::string, std::map<std::string, std::vector<KLV*> > > > m_detailedTriggerMatchedJets;
-	std::map<KJet*, std::map<std::string, std::map<std::string, std::vector<KLV*> > > > m_detailedTriggerMatchedTaggedJets;
+	std::map<KElectron*, std::map<std::string, std::map<std::string, std::vector<KLV> > > > m_detailedTriggerMatchedElectrons;
+	std::map<KMuon*, std::map<std::string, std::map<std::string, std::vector<KLV> > > > m_detailedTriggerMatchedMuons;
+	std::map<KTau*, std::map<std::string, std::map<std::string, std::vector<KLV> > > > m_detailedTriggerMatchedTaus;
+	std::map<KBasicJet*, std::map<std::string, std::map<std::string, std::vector<KLV> > > > m_detailedTriggerMatchedJets;
+	std::map<KJet*, std::map<std::string, std::map<std::string, std::vector<KLV> > > > m_detailedTriggerMatchedTaggedJets;
 
-	std::map<KLepton*, std::map<std::string, std::map<std::string, std::vector<KLV*> > >* > m_detailedTriggerMatchedLeptons;
+	std::map<KLepton*, std::map<std::string, std::map<std::string, std::vector<KLV> > >* > m_detailedTriggerMatchedLeptons;
 	std::map<KLepton*, std::map<std::string, bool > > m_detailedL1MatchedLeptons;
 
 	/// added by TriggerMatchingProducer
