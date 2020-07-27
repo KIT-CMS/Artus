86b01c8efb928db2d85b0ee43b254ffe079c8c45
Maximilian Maria Horzela <maximilian.horzela@kit.edu>
2020-07-27 12:17:30 +0200

    [MuonCorrectionsProducer] Corrected Rochester Correction check for UL17 and 2018

--- KappaAnalysis/src/Producers/MuonCorrectionsProducer.cc
+++ KappaAnalysis/src/Producers/MuonCorrectionsProducer.cc
@@ -14,7 +14,8 @@ MuonCorrectionsProducer::MuonEnergyCorrection MuonCorrectionsProducer::ToMuonEne
 	else if (muonEnergyCorrection == "rochcorr2015") return MuonCorrectionsProducer::MuonEnergyCorrection::ROCHCORR2015;
 	else if (muonEnergyCorrection == "rochcorr2016") return MuonCorrectionsProducer::MuonEnergyCorrection::ROCHCORR2016;
 	else if (muonEnergyCorrection == "rochcorr2017") return MuonCorrectionsProducer::MuonEnergyCorrection::ROCHCORR2017;
-	else if (muonEnergyCorrection == "rochcorr2017") return MuonCorrectionsProducer::MuonEnergyCorrection::ROCHCORR2017;
+	else if (muonEnergyCorrection == "rochcorr2017ul") return MuonCorrectionsProducer::MuonEnergyCorrection::ROCHCORR2017UL;
+	else if (muonEnergyCorrection == "rochcorr2018") return MuonCorrectionsProducer::MuonEnergyCorrection::ROCHCORR2018;
 	else return MuonCorrectionsProducer::MuonEnergyCorrection::NONE;
 }
 
